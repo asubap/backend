301454957ed205639fc78183915a85b8574849a4
ashcodex505 <ashishkurse@gmail.com>
2025-04-13 16:42:04 -0700

    couple more changes just making comments

--- src/routes/sponsor.ts
+++ src/routes/sponsor.ts
@@ -18,12 +18,12 @@ const controller = new SponsorController();
 
 // Public routes (no authentication required)
 router
-.post("/add-sponsor", verifySupabaseToken, controller.addSponsor.bind(controller)) // add sponsor-email and assign role
+.post("/add-sponsor", verifySupabaseToken, controller.addSponsor.bind(controller)) // add new sponsor and send email to all recruiters 
  // delete user-email
 .get("/", controller.getAllSponsors.bind(controller)) // get all sponsors
 .get("/:companyName/resources", controller.getSponsorResources.bind(controller)) // get sponsor resources by company name
 
-.post("/add-sponsor", controller.addSponsor.bind(controller)) // add a new sponsor;
+
 .post("/get-sponsor-info",controller.getSponsorByPasscode.bind(controller)) // get sponsor info by company name;
 
 // Sponsor Details Management
