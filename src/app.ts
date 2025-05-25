6c025366fed6dd08dccaefef7cb9be5aa2d6f595
asubap <tech.asubap@gmail.com>
2025-05-24 17:44:45 -0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,8 @@ const app = express();
 app.use(cors({
     origin: [
         "https://frontend-iota-gules-58.vercel.app",
-        "http://localhost:3000",
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://www.asubap.com
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
