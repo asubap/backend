4cbe6ea25a1754339c4dac55867b9208e9dc89fc
Ashish Kurse <98429783+ashcodex505@users.noreply.github.com>
2025-08-29 12:32:32 -0700

    Fix typo in getUserIdByEmail method

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -88,7 +88,7 @@ export default class UserRoleService {
         // Use service role client for admin API
         const adminClient = createSupabaseClient(undefined, true);
         console.log(adminClient);
-        const { data: userData, error: userError } = await adminClient.auth.admin
+        const { data: userData, error: userError } = await adminClient.auth.admin.listUsers();
         if (userError) throw userError;
         console.log(userData);
         const user = userData.users.find(u => u.email === user_email);
